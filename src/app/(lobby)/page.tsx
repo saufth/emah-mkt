5f3338e15c40ed9b0327a0abdecde16d598d058e
saufth <saufth@gmail.com>
2023-09-11 12:12:14 -0600

    TypeAnimation added

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from '@/components/TypeAnimation'
 import { Badge } from '@/components/ui/Badge'
 import { Button } from '@/components/ui/Button'
 import { Balancer } from 'react-wrap-balancer'
@@ -16,8 +17,8 @@ export default function Home () {
             </Badge>
             <Balancer className='font-cairo text-center space-y-6 relative'>
               {/* <div className='w-full h-4 inset-0 m-auto bg-green-500 absolute top-0 -z-10 before:absolute before:h-[100px] before:w-full before:bg-red-300' /> */}
-              <h1 className='text-5xl sm:text-6xl lg:text-7xl -tracking-wide lg:leading-[5rem] font-black bg-clip-text text-transparent bg-gradient-to-t from-black to-zinc-700 dark:from-zinc-200 dark:to-white'>
-                El orden de los factores <span className='text-emah'>Si</span> altera el producto
+              <h1 className='text-5xl sm:text-6xl lg:text-7xl -tracking-wide lg:leading-[5rem] font-black text-foreground'>
+                El orden de los factores <br className='hidden sm:block' /><span className='text-emah'>Si</span> <br className='sm:hidden' /><TypeAnimation sequence={[1000, 'altera el producto', 2000, 'beneficia al cliente', 2000, 'aporta valor', 1000]} speed={50} repeat={Infinity} />
               </h1>
               <p className='text-xl text-muted-foreground tracking-wider leading-relaxed'>
                 Impulsamos a emprendedores y empresarios que ofrecen productos intangibles de alto
---
+++ src/components/TypeAnimation.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { TypeAnimation as ReactTypeAnimation, type Props as TypeAnimationProps } from 'react-type-animation'
+
+export function TypeAnimation (props: TypeAnimationProps) {
+  return (
+    <ReactTypeAnimation {...props} />
+  )
+}
