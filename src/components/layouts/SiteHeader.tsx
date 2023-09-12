a1dce40fd77441ec75addca932fbfd335f0a50fc
saufth <saufth@gmail.com>
2023-09-11 18:00:54 -0600

    Hero gradient radial bg fixed

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -6,16 +6,19 @@ import { Button } from '../ui/Button'
 
 export default async function SiteHeader () {
   return (
-    <header className='w-full fixed top-0 z-40 border-b bg-white/30 dark:bg-black/20 backdrop-blur-sm backdrop-saturate-200'>
-      <div className='max-w-8xl h-20 mx-auto px-4 flex items-center justify-between lg:px-8'>
+    <header className='w-full fixed top-0 z-40 border-b bg-white/40 dark:bg-black/30 backdrop-blur-sm backdrop-saturate-200'>
+      <div className='max-w-8xl h-16 sm:h-20 mx-auto px-4 flex items-center justify-between lg:px-8'>
         <Link
           href='/'
         >
           <Icons.Logoname className='w-auto h-9 fill-primary hidden sm:block' />
-          <Icons.Logomark className='w-auto h-9 fill-primary block sm:hidden' />
+          <Icons.Logomark className='w-auto h-8 fill-primary block sm:hidden' />
         </Link>
-        <div className='flex justify-end gap-x-4'>
-          <Button className='highlight-white/20'>
+        <div className='flex justify-end items-center gap-x-4'>
+          <Button size='lg' className='hidden md:block'>
+            Contáctanos
+          </Button>
+          <Button size='sm' className='md:hidden'>
             Contáctanos
           </Button>
           <ThemeToggle />
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -22,9 +22,9 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        full: 'w-full sm:w-auto h-10 rounded-md sm:px-8 text-center',
+        sm: 'h-8 rounded-md px-4 text-xs',
+        lg: 'h-11 rounded-md px-6',
+        full: 'w-full sm:w-auto h-11 rounded-md sm:px-6 text-center',
         icon: 'h-9 w-9'
       }
     },
