56e8c944f17590ad2f9f22746575fc0284d44c60
saufth <saufth@gmail.com>
2023-09-12 16:38:58 -0600

    Mteors added

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,7 +1,9 @@
-import { TypeAnimation } from '@/components/TypeAnimation'
 import { Badge } from '@/components/ui/Badge'
 import { Button } from '@nextui-org/button'
+import dynamic from 'next/dynamic'
 import { Balancer } from 'react-wrap-balancer'
+
+const Meteors = dynamic(() => import('@/components/Meteors'), { ssr: false })
 
 export default function Home () {
   return (
@@ -9,14 +11,14 @@ export default function Home () {
       <section className='relative'>
         <div className='absolute w-2/3 md:w-1/2 lg:w-1/4 h-md translate top-16 right-0 left-0 mx-auto rounded-full bg-gradient-conic from-sky-200 via-blue-200 blur-2xl content-[""] opacity-10 dark:from-sky-900/40 dark:via-blue-600/40' />
         <div className='absolute w-1/2 lg:w-sm h-sm translate top-6 sm:top-12 left-0 right-0 mx-auto rounded-full bg-gradient-radial from-emah dark:from-emah/40 dark:to-blue-700/30 to-transparent blur-2xl content-[""] opacity-10' />
-        <div className='max-w-7xl px-4 md:px-6 mx-auto pt-8 sm:pt-36 relative'>
+        <div className='max-w-7xl px-4 md:px-6 mx-auto pt-8 sm:pt-36 relative z-10'>
           <div className='flex flex-col items-center gap-y-6'>
             <Badge className='rounded-md px-3.5 py-1.5' variant='secondary'>
               ANTI-MARKETING
             </Badge>
             <Balancer className='font-cairo text-center space-y-6'>
               <h1 className='text-[2.75rem] leading-none sm:text-6xl lg:text-7xl -tracking-wide lg:leading-[5rem] font-black text-foreground'>
-                El orden de los factores <br className='hidden sm:block' /><span className='text-emah'>Sí</span> <br className='sm:hidden' /><TypeAnimation sequence={[100, 'altera el producto']} speed={50} cursor={false} />
+                El orden de los factores <span className='text-emah'>Sí</span> altera el producto
               </h1>
               <p className='text-xl text-muted-foreground tracking-wider leading-relaxed'>
                 Impulsamos a emprendedores y empresarios que ofrecen productos intangibles de alto
@@ -29,6 +31,9 @@ export default function Home () {
           </div>
         </div>
       </section>
+      <div className='w-full h-full absolute top-0 overflow-hidden'>
+        <Meteors />
+      </div>
       {/* <video
         className='w-9xl h-auto object-cover object-center aspect-video'
         width={1920}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,27 +80,25 @@ const config: Config = {
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        }
-      },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'meteor-effect': 'meteor 5s linear infinite'
+      },
+      keyframes: {
+        meteor: {
+          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
+          '70%': { opacity: '1' },
+          '100%': {
+            transform: 'rotate(215deg) translateX(-500px)',
+            opacity: '0'
+          }
+        }
       }
     }
   },
